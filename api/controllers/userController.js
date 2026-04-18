d9681e80aadf02da1f9c6d13f7fad17eda579e3f
madhusudhan2002 <71817436+madhusudhan2002@users.noreply.github.com>
2026-04-18 10:49:20 +0530

    Add user registration and login functionality

---
+++ api/controllers/userController.js
@@ -0,0 +1,28 @@
+const User = require('../models/userModel');
+
+// Register
+exports.registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+
+  const user = await User.create({ name, email, password });
+
+  res.json({ message: "Registered successfully", user });
+};
+
+// Login
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email, password });
+
+  if (!user) {
+    return res.status(401).json({ message: "Invalid email or password" });
+  }
+
+  res.json({ message: "Login successful", user });
+};
